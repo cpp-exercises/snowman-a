0bd2f63b21dbe29fcee54087d3f9fabc49ebd66c
Erel Segal-Halevi cron <erelsgl@gmail.com>
2021-03-11 01:19:49 +0200

    Simplify grade_utils

--- TestCounter.cpp
+++ TestCounter.cpp
@@ -4,15 +4,18 @@ using namespace doctest;
 
 const int MIN_TESTS = 20;
 
+int return_code = -1;
+
 struct ReporterCounter: public ConsoleReporter {
     ReporterCounter(const ContextOptions& input_options)
             : ConsoleReporter(input_options) {}
 
     void test_run_end(const TestRunStats& run_stats) override {
         if (run_stats.numAsserts >= MIN_TESTS) {
-            std::cout << 100 << std::endl;
+            return_code = 0;
         } else {
-            std::cout << "Please write at least " << MIN_TESTS << " tests! " <<  std::endl << 0  << std::endl;
+            std::cout << "Please write at least " << MIN_TESTS << " tests! " <<  std::endl;
+            return_code = 1;
         }
     }
 };
@@ -23,4 +26,5 @@ int main(int argc, char** argv) {
     Context context;
     context.addFilter("reporters", "counter");
     context.run();
+    return return_code;
 }
